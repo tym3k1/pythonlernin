70369d77aa91bb66c23cf0a2e1fdd9d61f60620e
Tymoteusz Luczko <tymoteusz.luczko@gmail.com>
2021-05-08 18:25:55 +0200

    wsio

--- new_project/lab8panda.py
+++ new_project/lab8panda.py
@@ -9,23 +9,45 @@ import openpyxl
 
 xlsx = pd.ExcelFile('new_project/imiona.xlsx')
 dataframe = pd.read_excel(xlsx)
-print(dataframe)
+#print(dataframe)
 #2-1
-print(dataframe[dataframe['Liczba']>1000])
+#print(dataframe[dataframe['Liczba']>1000])
 #2-2
-print(dataframe[dataframe['Imie']=='TYMOTEUSZ'])
+#print(dataframe[dataframe['Imie']=='TYMOTEUSZ'])
 #2-3
-print(dataframe.agg({'Liczba':['sum']}))
+#print(dataframe.agg({'Liczba':['sum']}))
 #2-4
-datka = dataframe.groupby(['Rok']).agg({'Liczba':['sum']})
-datka=datka[:6]
-print(datka)
+#print(dataframe[((dataframe['Rok']<=2005) & (dataframe['Rok']>=2000))].agg({'Liczba':['sum']}))
 #2-5
-print(dataframe.groupby(['Plec']).agg({'Liczba':['sum']}))
+#print(dataframe.groupby(['Plec']).agg({'Liczba':['sum']}))
 #2-6
-nowadat = dataframe.groupby(['Rok', 'Plec']).head(1)
-print(dataframe.groupby(['Rok', 'Plec']).head(1))
+
+#print(dataframe.sort_values('Liczba', ascending=False).groupby(['Rok', 'Plec']).nth(0))
 #2-7
-nownow = nowadat.groupby(['Imie']).agg({'Liczba':['sum']})
-nownow=nownow[1:3]
-print(nownow)
+
+#print(dataframe[dataframe['Plec'] == 'M'].groupby(['Imie']).agg({'Liczba':['sum']}).sort_values(('Liczba','sum'),ascending=False).iloc[0])
+#print(dataframe[dataframe['Plec'] == 'K'].groupby(['Imie']).agg({'Liczba':['sum']}).sort_values(('Liczba','sum'),ascending=False).iloc[0])
+#print(dataframe.groupby(['Plec', 'Imie']).agg({'Liczba':['sum']}).sort_values(('Liczba','sum'), ascending=False).iloc[[0,1]])
+
+df = pd.read_csv('new_project\kotlecik.csv', delimiter=';')
+print(df)
+# print(df.dtypes)
+#3-1
+#print(df['Sprzedawca'].unique())
+print(set(df['Sprzedawca']))
+#3-2
+print(df.sort_values('Utarg', ascending=False).head())
+#3-3
+print(df.groupby(['Sprzedawca']).agg({'idZamowienia':['count']}))
+#3-4
+print(df.groupby(['Kraj']).size())
+#3-5
+print(df[(df['Data zamowienia'].str[0:4]=='2005') & (df['Kraj']=='Polska')].agg({'idZamowienia':['count']}))
+#3-6
+print(df[(df['Data zamowienia'].str[0:4]=='2005')].agg({'Utarg':['mean']}))
+#3-7
+zamowienia_2004 = df[(df['Data zamowienia'].str[0:4]=='2004')]
+zamowienia_2005 = df[(df['Data zamowienia'].str[0:4]=='2005')]
+
+zamowienia_2004.to_csv("zamowienia_2004.csv", index=False)
+zamowienia_2005.to_csv("zamowienia_2004.csv",sep='@', index=False)
---
+++ new_project/lab9matplot.py
@@ -0,0 +1,63 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+import xlrd
+import openpyxl
+
+xlsx = pd.ExcelFile('new_project/imiona.xlsx')
+dataframe = pd.read_excel(xlsx)
+
+
+def zad1():
+    imionka = dataframe.groupby(['Rok']).agg({'Liczba':['sum']})
+    imionka.plot()
+    plt.show()
+    
+def zad2():
+    plec = dataframe.groupby(['Plec']).agg({'Liczba':['sum']})
+    wykres = plec.plot.bar()
+    wykres.set_ylabel('Ilosc')
+    plt.title('Urodzenia dzieci 00-17 plec')
+    plt.show()
+
+def zad3():
+    ostatnie_piec = dataframe[dataframe['Rok'] >= 2013].groupby(['Plec']).agg({'Liczba': ['sum']})
+    kolko = ostatnie_piec.plot.pie(subplots=True, autopct='%.2f %%', fontsize=20, figsize=(7,7))
+    plt.title('Dzieci ale chyba nie tak')
+    plt.show()
+
+""" 
+irisone = df[df['class']=='Iris-setosa']
+    iristwo = df[df['class']=='Iris-versicolor']
+    iristhree = df[df['class']=='Iris-virginica']
+    irisone.plot(kind="scatter", x="SL", y="SW")
+    iristwo.plot(kind="scatter", x="SL", y="SW")
+    iristhree.plot(kind="scatter", x="SL", y="SW") """
+
+
+def zad4():
+    df = pd.read_csv('new_project\iris.csv', delimiter=',', names=['SL','SW','PL','PW','class'])
+    colors = {'Iris-setosa': 'red', 'Iris-versicolor': 'blue', 'Iris-virginica': 'green'}
+    scatter_col = [colors[i] for i in df['class']]
+    plt.scatter(df['SL'], df['SW'],c=scatter_col)
+    plt.title("sepal width vs sepal length")
+    plt.show()
+ 
+
+
+def zad5():
+    df = pd.read_csv('new_project\kotlecik.csv', delimiter=';')
+    newdata = df.groupby(['Sprzedawca']).agg({'idZamowienia':['count']})
+    wykres = newdata.plot.bar()
+    wykres.set_ylabel('Ilosc zamowien')
+    wykres.set_xlabel('Sprzedawcy')
+    plt.title('Ilosc zamowien xD')
+    plt.show()
+
+zad5()
+#zad4()
+#zad3()
+#zad2()
+#zad1()
